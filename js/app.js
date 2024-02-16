5ba3f97cec835d434ea0f5da3c94cf3385752bd8
Abdulqadir8604 <web.aq8604@gmail.com>
2024-02-16 15:35:15 +0530

    styled

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-const gregorianDate = document.getElementById('gregorianDate');
+const QgregorianDate = document.getElementById('gregorianDate@');
 const misriDate = document.getElementById('misriDate');
+const misriDatev1 = document.getElementById('misriDatev1');
 
-gregorianDate.addEventListener('change', () => {
-  const gregorianDateValue = gregorianDate.value;
+const QmisriDate = document.getElementById('misriDate@');
+const gregorianDate = document.getElementById('gregorianDate');
+
+  QgregorianDate.addEventListener('change', () => {
+  const gregorianDateValue = QgregorianDate.value;
   const formattedDate = gregorianDateValue.split('-');
   const year = formattedDate[0];
   const month = formattedDate[1];
@@ -10,6 +14,11 @@ gregorianDate.addEventListener('change', () => {
   const date = `${day}-${month}-${year}`;
   convertToMisriDate(date);
 });
+
+QmisriDate.addEventListener('change', () => {
+  const misriDateValue = QmisriDate.value;
+  convertToGregorianDate(misriDateValue);
+})
 const convertToMisriDate = (date) => {
   const url = `https://misri-date-api.onrender.com/gregToMisri/${date}`;
   fetch(url, {
@@ -19,7 +28,21 @@ const convertToMisriDate = (date) => {
     },
   }).then(r => r.json())
   .then(data => {
-    misriDate.value = data.data.misriDatev1;
+    misriDate.value = data.data.misriDatev2;
+    misriDatev1.innerText = data.data.misriDatev1;
+  })
+}
+
+const convertToGregorianDate = (date) => {
+  const url = `https://misri-date-api.onrender.com/misriToGreg/${date}`;
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(r => r.json())
+  .then(data => {
+    gregorianDate.value = data.data.gregorianDatev1;
   })
 }
 
